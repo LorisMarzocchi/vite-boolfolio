94b419f78414d45ee593fc0494f59049e6a00b14
LorisMarzocchi <ml77182a@gmail.com>
2023-07-17 19:27:53 +0200

    add project show

--- src/pages/ProjectsShow.vue
+++ src/pages/ProjectsShow.vue
@@ -6,19 +6,37 @@ export default {
   data() {
     return {
       store,
+      project: null,
     };
   },
+  methods: {
+    getImageUrl(image) {
+      return image
+        ? this.store.baseUrl + "storage/" + image
+        : this.store.baseUrl + "storage/defaultImage/default.jpg";
+    },
+  },
   created() {
-    // fai la richiesta axios
     axios
       .get(this.store.baseUrl + "api/projects/" + this.$route.params.slug)
-      .then((response) => response.data.data);
+      .then((response) => {
+        this.project = response.data;
+      });
   },
 };
 </script>
 
 <template>
-  <h2>show</h2>
+  <div>
+    <h2>Project</h2>
+    <div v-if="project">
+      <h3 class="text-danger">{{ project.title }}</h3>
+      <p>{{ project.description }}</p>
+    </div>
+    <div v-else>
+      <p>Caricamento...</p>
+    </div>
+  </div>
 </template>
 
 <style></style>
